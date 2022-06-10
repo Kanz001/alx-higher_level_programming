84c5372355dabc499ad2d1d0e3a9ef3a40a7d250
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-06-10 12:25:12 +0100

    Add readme for 4-hidden_discovery.py and Hot fix

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    n = len(sys.argv)
+    result = 0
+    for i in range(n):
+        if i == 0:
+	    continue
+        result += int(sys.argv[i])
+print(result)

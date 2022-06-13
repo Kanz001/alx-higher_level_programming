49637c59fd5cbf982f488a8b01e1f7314f04cf99
Kanz001 <abdulhameedabdulfatai@gmail.com>
2022-06-13 03:01:56 +0100

    Hot fix

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    output = []
+
+    for i in range(len(my_list)):
+	if my_list[i] % 2 == 0:
+	    output.append(True)
+	else:
+	    output.append(False)
+
+    return (output)
